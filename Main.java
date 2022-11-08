06b24dfd639df55fc6cb27c8dd61884c6c8f9cb1
村守 <s.muramori.sys22@morijyobi.ac.jp>
2022-11-08 10:17:39 +0900

    Main.javaðύXêñÚ

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 	public class Main{ 
 		public static void main(String[] args){
 			Scanner sc = new Scanner(System.in);
+			System.out.println("1人目");
 			System.out.print("身長:");
 			int hight = sc.nextInt();
 			System.out.print("体重");
@@ -12,6 +13,17 @@ import java.util.Scanner;
 			bmi *=10000;
 			BigDecimal bd = new BigDecimal(bmi);
 			bd = bd.setScale(2,RoundingMode.DOWN);
-			System.out.println(bd);
+			System.out.println("BMI" + bd);
+			
+			System.out.println("2人目");
+			System.out.print("身長:");
+			int hight2 = sc.nextInt();
+			System.out.print("体重");
+			int weight2 = sc.nextInt();
+			double bmi2 = weight2 / Math.pow(hight2,2);
+			bmi2 *=10000;
+			BigDecimal bd2 = new BigDecimal(bmi2);
+			bd2 = bd.setScale(2,RoundingMode.DOWN);
+			System.out.println("BMI" + bd2);
 		}
 	}
