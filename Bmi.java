5798fd89ae4b0c5a776d3c42bb60322ac0068cea
村守 <s.muramori.sys22@morijyobi.ac.jp>
2022-11-11 10:24:07 +0900

    Main.javaをBmi.javaに変更

--- Bmi.java
+++ Bmi.java
@@ -1,37 +1,14 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+
 import java.util.Scanner;
-	public class Bmi{ 
-		public static void main(String[] args){
-			Scanner sc = new Scanner(System.in);
-<<<<<<< HEAD
-			System.out.println("1人目:");
-=======
-			System.out.println("1人目");
->>>>>>> 06b24dfd639df55fc6cb27c8dd61884c6c8f9cb1
-			System.out.print("身長:");
-			int hight = sc.nextInt();
-			System.out.print("体重");
-			int weight = sc.nextInt();
-			double bmi = weight / Math.pow(hight,2);
-			bmi *=10000;
-			BigDecimal bd = new BigDecimal(bmi);
-			bd = bd.setScale(2,RoundingMode.DOWN);
-			System.out.println("BMI" + bd);
-			
-<<<<<<< HEAD
-			System.out.println("2人目:");
-=======
-			System.out.println("2人目");
->>>>>>> 06b24dfd639df55fc6cb27c8dd61884c6c8f9cb1
-			System.out.print("身長:");
-			int hight2 = sc.nextInt();
-			System.out.print("体重");
-			int weight2 = sc.nextInt();
-			double bmi2 = weight2 / Math.pow(hight2,2);
-			bmi2 *=10000;
-			BigDecimal bd2 = new BigDecimal(bmi2);
-			bd2 = bd.setScale(2,RoundingMode.DOWN);
-			System.out.println("BMI" + bd2);
-		}
-	}
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0 ;i < 2;i++){
+        double  sin = scan.nextDouble();
+        double tai = scan.nextDouble();
+        double sin2 = sin /100; 
+        double bmi = tai / (sin2 * sin2);
+        System.out.println(String.format("%.2f", bmi));
+        }
+    }
+}
